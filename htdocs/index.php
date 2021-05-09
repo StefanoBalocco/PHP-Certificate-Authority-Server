d734c4b355b8874d9c8b8e91bd490a3b8e3e7071
Stefano Balocco <stefano.balocco@gmail.com>
2021-05-09 13:34:55 +0200

    Separated php files from data files: certstore is now defaulted to www_root/../certstore

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,29 +1,20 @@
-<?PHP
-if (session_id() === '') {
-    session_start();
-}
+<?php
+session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-include("./include/functions.php");
-include("./include/settings.php");
+include('./include/functions.php');
+include('./include/settings.php');
 //update config
 $config = update_config();
 $_SESSION['config']=$config;
 
-include("./include/functions_setup.php");
-include("./include/ssl-functions.php");
-include("./include/functions_csr.php");
-include("./include/functions_cert.php");
-include("./include/functions_key.php");
-include("./include/functions_ca.php");
-include("./include/functions_show_summary.php");
-include("./include/functions_misc.php");
-
-
+include('./include/functions_setup.php');
+include('./include/ssl-functions.php');
+include('./include/functions_csr.php');
+include('./include/functions_cert.php');
+include('./include/functions_key.php');
+include('./include/functions_ca.php');
+include('./include/functions_show_summary.php');
+include('./include/functions_misc.php');
 
 $_SESSION['cwd'] = dirname(__FILE__);
 $page_variables=array();
@@ -99,6 +90,4 @@ if (isset($page_variables['cert_dn']['keySize'])) {
 // =================================================================================================================================================================
 
 //this will call header and footer
-include("menu_switch.php");
-
-?>
+include('menu_switch.php');
